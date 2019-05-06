85296fe4202c0c8c662bc5f4a8f4dce5ca957f2a
Steph <stephaniergreene94@gmail.com>
2019-05-06 20:24:21 +1000

    arrays advancement with ES6

--- starter/script.js
+++ starter/script.js
@@ -223,7 +223,7 @@ Person.prototype.myFriends5 = function(friends) {
 new Person('Mike').myFriends5(friends);
 */
 
-//////////////////////////////////
+////////////////////////////////// https://www.linkedin.com/in/steph-greene/
 //// Lecture: Decontructing
 
 // ES5
@@ -261,5 +261,60 @@ calcAgeRetirement(1990);
 console.log(age3)
 console.log(retirement);
 
+//////////////////////////////////
+//// Lecture: Arrays
+
+const boxes = document.querySelectorAll('.box');
+
+
+//ES5
+var boxesArr5 = 
+Array.prototype.slice.call(boxes);
+boxesArr5.forEach(function(cur) {
+    cur.style.backgroundColor =
+    'dodgerblue';
+});
+
+
+//ES6
+const boxesArr6 = Array.from(boxes);
+boxesArr6.forEach(cur => cur.style.backgroundColor = 'dodgerblue');
+
+//change text
+//ES5
+/*
+for(var i =0; i < boxesArr5.length; i++) {
+    if(boxesArr5[i].className === 'box blue') {
+        continue;
+    }
+
+    boxesArr5[i].textContent = 'I changed to blue!';
+}
+*/
+
+//ES6
+for (const cur of boxesArr6) {
+    if (cur.className.includes('blue')) {
+        continue;
+    }
+    cur.textContent = 'I changed to blue!';
+}
+
+//find elements in an array
+//ES5
+var ages = [12, 17, 8, 21, 14, 11];
+//boolan array
+
+var full = ages.map(function(cur) {
+    return cur >= 18;
+});
+console.log(full);
+
+console.log(full.indexOf(true));
+console.log(ages[full.indexOf(true)]);
+
+//ES6
+console.log(ages.findIndex(cur => cur >= 18));
+console.log(ages.find(cur => cur >= 18));
 
 
